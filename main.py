5d430d53b9ff594350d30d1a5e891ea3fe37b205
ilyass <ex_p@live.com>
2023-08-25 12:39:25 +0300

    spiral drawer

--- main.py
+++ main.py
@@ -1,112 +1,24 @@
-import random
+import pyautogui
+import time
 
-DICE_ART = {
-    1: (
-        "┌─────────┐",
-        "│         │",
-        "│    ●    │",
-        "│         │",
-        "└─────────┘",
-    ),
-    2: (
-        "┌─────────┐",
-        "│  ●      │",
-        "│         │",
-        "│      ●  │",
-        "└─────────┘",
-    ),
-    3: (
-        "┌─────────┐",
-        "│  ●      │",
-        "│    ●    │",
-        "│      ●  │",
-        "└─────────┘",
-    ),
-    4: (
-        "┌─────────┐",
-        "│  ●   ●  │",
-        "│         │",
-        "│  ●   ●  │",
-        "└─────────┘",
-    ),
-    5: (
-        "┌─────────┐",
-        "│  ●   ●  │",
-        "│    ●    │",
-        "│  ●   ●  │",
-        "└─────────┘",
-    ),
-    6: (
-        "┌─────────┐",
-        "│  ●   ●  │",
-        "│  ●   ●  │",
-        "│  ●   ●  │",
-        "└─────────┘",
-    ),
-}
-DIE_HEIGHT = len(DICE_ART[1])
-DIE_WIDTH = len(DICE_ART[1][0])
-DIE_FACE_SEPARATOR = " "
+time.sleep(3)
 
-def parse_input(input_string):
-    if input_string.strip() in {"1", "2", "3", "4", "5", "6"}:
-        return int(input_string)
-    else:
-        print("Please enter a number from 1 to 6.")
-        raise SystemExit(1)
+print(pyautogui.size())
+print(pyautogui.position())
+
+pyautogui.moveTo(100,100,1)
+pyautogui.moveRel(100,100)
 
 
-def roll_dice(num_dice):
-    roll_results = []
-    for _ in range(num_dice):
-        roll = random.randint(1, 6)
-        roll_results.append(roll)
-    return roll_results
+time.sleep(1)
+distance=400
+a=distance
 
+while distance>50:
+    pyautogui.dragRel(distance,0,1,button="left")
+    distance -= 50
+    pyautogui.dragRel(0,distance,1,button="left")
+    pyautogui.dragRel(-distance,0,1,button="left")
+    distance -= 50
+    pyautogui.dragRel(0,-distance,1,button="left")
 
-def generate_dice_faces_diagram(dice_values):
-    """Return an ASCII diagram of dice faces from `dice_values`.
-
-    The string returned contains an ASCII representation of each die.
-    For example, if `dice_values = [4, 1, 3, 2]` then the string
-    returned looks like this:
-
-    ~~~~~~~~~~~~~~~~~~~ RESULTS ~~~~~~~~~~~~~~~~~~~
-    ┌─────────┐ ┌─────────┐ ┌─────────┐ ┌─────────┐
-    │  ●   ●  │ │         │ │  ●      │ │  ●      │
-    │         │ │    ●    │ │    ●    │ │         │
-    │  ●   ●  │ │         │ │      ●  │ │      ●  │
-    └─────────┘ └─────────┘ └─────────┘ └─────────┘
-    """
-    dice_faces = _get_dice_faces(dice_values)
-    dice_faces_rows = _generate_dice_faces_rows(dice_faces)
-
-    width = len(dice_faces_rows[0])
-    diagram_header = " RESULTS ".center(width, "~")
-
-    dice_faces_diagram = "\n".join([diagram_header] + dice_faces_rows)
-    return dice_faces_diagram
-
-
-def _get_dice_faces(dice_values):
-    dice_faces = []
-    for value in dice_values:
-        dice_faces.append(DICE_ART[value])
-    return dice_faces
-
-
-def _generate_dice_faces_rows(dice_faces):
-    dice_faces_rows = []
-    for row_idx in range(DIE_HEIGHT):
-        row_components = []
-        for die in dice_faces:
-            row_components.append(die[row_idx])
-        row_string = DIE_FACE_SEPARATOR.join(row_components)
-        dice_faces_rows.append(row_string)
-    return dice_faces_rows
-
-num_dice_input = input("How many dice do you want to roll? [1-6] ")
-num_dice = parse_input(num_dice_input)
-roll_results = roll_dice(num_dice)
-dice_face_diagram = generate_dice_faces_diagram(roll_results)
-print(f"\n{dice_face_diagram}")

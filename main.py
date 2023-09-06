f3d200cda72cbbe3eb1e55e806b5665b465bf53f
ilyass <ex_p@live.com>
2023-09-06 12:22:57 +0300

    pygame background changer

--- main.py
+++ main.py
@@ -1,24 +1,52 @@
-import pyautogui
+import pygame
 import time
+import os
 
-time.sleep(3)
+run = True
+WIDTH, HEIGHT = 1000, 500
+FPS = 60
 
-print(pyautogui.size())
-print(pyautogui.position())
-
-pyautogui.moveTo(100,100,1)
-pyautogui.moveRel(100,100)
+# Red Green and Blue
+WIN = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Pong")
 
 
-time.sleep(1)
-distance=400
-a=distance
+def red():
+    WIN.fill((250, 0, 0))
+    pygame.display.update()
+    time.sleep(1)
 
-while distance>50:
-    pyautogui.dragRel(distance,0,1,button="left")
-    distance -= 50
-    pyautogui.dragRel(0,distance,1,button="left")
-    pyautogui.dragRel(-distance,0,1,button="left")
-    distance -= 50
-    pyautogui.dragRel(0,-distance,1,button="left")
 
+def green():
+    WIN.fill((0, 250, 0))
+    pygame.display.update()
+    time.sleep(1)
+
+
+def blue():
+    WIN.fill((0, 0, 250))
+    pygame.display.update()
+    time.sleep(1)
+
+
+def rgb():
+    while run == True:
+        red()
+        green()
+        blue()
+
+
+def main():
+    rgb()
+    clock = pygame.time.Clock()
+    run = True
+    while run == True:
+        clock.tick(FPS)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False
+                pygame.quit()
+
+
+if __name__ == "__main__":
+    main()

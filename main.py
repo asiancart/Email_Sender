d38c749541a84d764a6540741db2d817c27d425e
ilyass <ex_p@live.com>
2023-09-25 13:29:14 +0300

    e mail sender

--- main.py
+++ main.py
@@ -1,52 +1,15 @@
-import pygame
-import time
-import os
+import smtplib as s
 
-run = True
-WIDTH, HEIGHT = 1000, 500
-FPS = 60
+ob = s.SMTP("smtp.gmail.com",587)
+ob.ehlo()
+ob.starttls()
+ob.login("sender@gmail.com","passwordhere")
+subject="test python"
+body="i love python"
+message="subject:{}\n\n{}".format(subject,body)
+listadd=["whichemailtosendadd@gmail.com",
+         "whichemailtosendadd2@gmail.com"]
 
-# Red Green and Blue
-WIN = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption("Pong")
-
-
-def red():
-    WIN.fill((250, 0, 0))
-    pygame.display.update()
-    time.sleep(1)
-
-
-def green():
-    WIN.fill((0, 250, 0))
-    pygame.display.update()
-    time.sleep(1)
-
-
-def blue():
-    WIN.fill((0, 0, 250))
-    pygame.display.update()
-    time.sleep(1)
-
-
-def rgb():
-    while run == True:
-        red()
-        green()
-        blue()
-
-
-def main():
-    rgb()
-    clock = pygame.time.Clock()
-    run = True
-    while run == True:
-        clock.tick(FPS)
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                run = False
-                pygame.quit()
-
-
-if __name__ == "__main__":
-    main()
+ob.sendmail("sender@gmail.com",listadd,message)
+print("send mail")
+ob.quit()
